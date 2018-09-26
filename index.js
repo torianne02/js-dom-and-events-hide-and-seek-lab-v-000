d27a22cf9ea692ca9ca2767cf237b9be9354da32
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-26 23:51:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let child = rankedLists[i].children;
+    for (let j = 0, k = child.length; j < k; j++) {
+      child[j].innerHTML = parseInt(child[j].innerHTML) + n;
+    }
+  }
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+
+  return node;
+}
