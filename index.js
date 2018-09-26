340f29d4f475663af672a8c70684944747004968
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-26 23:49:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
 
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let child = rankedLists[i].children;
+    for (let j = 0, k = child.length; j < k; j++) {
+      child[j].innerHTML = parseInt(child[j].innerHTML) + n;
+    }
+  }
 }
